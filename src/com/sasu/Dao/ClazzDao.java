bd7e85e0195a0ec819102d3d1104a25f1324968e
libin1999 <2414592137@qq.com>
2019-09-20 10:14:20 +0800

    李斌第一次提交

--- src/com/sasu/Dao/ClazzDao.java
+++ src/com/sasu/Dao/ClazzDao.java
@@ -1,14 +1,40 @@
 package com.sasu.Dao;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.sasu.Util.HibernateSessionFactory;
 import com.sasu.entity.ClazzInfo;
+import com.sasu.entity.StudentInfo;
 
 public class ClazzDao {
 	
 	public ClazzInfo getClazzById(int id) {
 		Session session=HibernateSessionFactory.getSession();
 		return session.get(ClazzInfo.class,id);
+	}
+	
+	public void save(ClazzInfo info) {
+		Session session=HibernateSessionFactory.getSession();
+		Transaction t=session.beginTransaction();//开启事物
+		session.save(info);
+		t.commit();	//提交事务
+		session.close();//关闭session对象
+	}
+	
+	public void update(ClazzInfo info) {
+		Session session=HibernateSessionFactory.getSession();
+		Transaction t=session.beginTransaction();
+		session.update(info);
+		t.commit();
+		session.close();
+	}
+	
+	public void delete(ClazzInfo info) {
+		Session session=HibernateSessionFactory.getSession();
+		Transaction t=session.beginTransaction();
+		session.delete(info);
+		t.commit();
+		session.close();
 	}
 }
--- src/com/sasu/entity/RoleUrlInfo.java
+++ src/com/sasu/entity/RoleUrlInfo.java
@@ -2,24 +2,25 @@ package com.sasu.entity;
 
 public class RoleUrlInfo {	//某种角色对应有哪些操作  李斌
 	private int id;
-	private int role_id;//角色编号
-	private int url_id;//url编号
+	private RoleInfo role_id;//角色编号
+	private OptUrl url_id;//url编号
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getRole_id() {
+	public RoleInfo getRole_id() {
 		return role_id;
 	}
-	public void setRole_id(int role_id) {
+	public void setRole_id(RoleInfo role_id) {
 		this.role_id = role_id;
 	}
-	public int getUrl_id() {
+	public OptUrl getUrl_id() {
 		return url_id;
 	}
-	public void setUrl_id(int url_id) {
+	public void setUrl_id(OptUrl url_id) {
 		this.url_id = url_id;
 	}
+	
 }
